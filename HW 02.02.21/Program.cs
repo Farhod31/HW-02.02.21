cf744af9e14a92ad830160af30c9a00117fcd6a2
Farhod31 <farhodmukhitdinov@mail.ru>
2021-02-03 23:04:15 +0300

    Добавьте файлы проекта.

---
+++ HW 02.02.21/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace hw_02_02._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Id = Convert.ToInt32(Console.ReadLine());
+            string FirstName = Console.ReadLine();
+            string LastName = Console.ReadLine();
+            string MiddleName = Console.ReadLine();
+            string Hobbies = Console.ReadLine();
+            Descriptions one = new Descriptions(Id, FirstName, LastName, MiddleName, Hobbies);
+            one.Description();
+        }
+    }
+    class Descriptions
+    {
+        public int Id;
+        public string FirstName;
+        public string LasteName;
+        public string MiddleName;
+        public string Hobbies;
+
+        public Descriptions(int Id, string FirstName, string LastName, string MiddleName, string Hobbies)
+        {
+            this.Id = Id;
+            this.FirstName = FirstName;
+            this.LasteName = LasteName;
+            this.MiddleName = MiddleName;
+            this.Hobbies = Hobbies;
+        }
+        public void Description()
+        {
+            Console.WriteLine($"Id: {Id}, \n FirstName:{FirstName}, \n LastName:{LasteName}, \n MiddleName: {MiddleName}, \n Hobbies: {Hobbies} ");
+        }
+    }
+
+}
